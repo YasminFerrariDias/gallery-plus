85e225000f99c5aa676f78c57224b8de6e16f15f
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-27 11:34:44 -0300

    Definindo comportamento do componente InputSingleFile - Aplicando validações

--- src/components/input-single-file.tsx
+++ src/components/input-single-file.tsx
@@ -40,10 +40,12 @@ interface InputSingleFileProps
   Omit<React.ComponentProps<"input">, "size"> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   form: any;
+  allowedExtensions: string[];
+  maxFileSizeInMB: number;
   error?: React.ReactNode;
 }
 
-export default function InputSingleFile({ form, size, error, ...props }: InputSingleFileProps) {
+export default function InputSingleFile({ form, size, error, allowedExtensions, maxFileSizeInMB, ...props }: InputSingleFileProps) {
   const formValues = useWatch({ control: form.control });
   const name = props.name || "";
   const formFile: File = React.useMemo(
@@ -51,17 +53,34 @@ export default function InputSingleFile({ form, size, error, ...props }: InputSi
     [formValues, name]
   )
 
+  const { fileExtension, fileSize } = React.useMemo(() => ({
+    fileExtension: formFile?.name?.split('.')?.pop()?.toLowerCase() || '',
+    fileSize: formFile?.size || 0
+  }), [formFile])
+
+  function isValidExtension() {
+    return allowedExtensions.includes(fileExtension);
+  }
+
+  function isValidSize() {
+    return fileSize <= maxFileSizeInMB * 1024 * 1024;
+  }
+
+  function isValidFile() {
+    return isValidExtension() && isValidSize();
+  }
+
   return (
     <div>
-      {!formFile ?
+      {!formFile || !isValidFile() ?
         (
           <>
             <div className="w-full relative group cursor-pointer">
               <input type="file"
                 className={`
-            absolute top-0 right-0 w-full h-full
-            opacity-0 cursor-pointer
-          `}
+                  absolute top-0 right-0 w-full h-full
+                  opacity-0 cursor-pointer
+                `}
                 {...props}
               />
               <div className={inputSingleFileVariants({ size })}>
@@ -73,11 +92,23 @@ export default function InputSingleFile({ form, size, error, ...props }: InputSi
                 </Text>
               </div>
             </div>
-            {error && (
-              <Text variant="label-small" className="text-accent-red">
-                Erro no campo!
-              </Text>
-            )}
+            <div className="flex flex-col gap-1 mt-1">
+              {formFile && !isValidExtension() &&
+                <Text variant="label-small" className="text-accent-red">
+                  Tipo de arquivo inválido!
+                </Text>
+              }
+              {formFile && !isValidSize() &&
+                <Text variant="label-small" className="text-accent-red">
+                  O tamanho do arquivo ultrapassa o máximo!
+                </Text>
+              }
+              {error && (
+                <Text variant="label-small" className="text-accent-red">
+                  {error}
+                </Text>
+              )}
+            </div>
           </>
         ) : (
 
