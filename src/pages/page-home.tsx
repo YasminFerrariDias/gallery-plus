adbf328d048b8132d0d4e793d801028459226adb
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-27 16:00:27 -0300

    não finalizado - Componente PhotoWidget

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,9 +1,23 @@
-import Text from "../components/text";
+import Container from "../components/container";
+import PhotoWidget from "../contexts/photos/components/photo-widget";
 
 export default function PageHome() {
   return (
-    <>
-      <Text variant="heading-large">Página inicial</Text>
-    </>
+    <Container>
+      <div className="grid grid-cols-4 gap-9">
+        <PhotoWidget
+          photo={{
+            id: '123',
+            title: "Olá mundo!",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: " 3421", title: "Album 1" },
+              { id: " 3426", title: "Album 2" },
+              { id: " 3341", title: "Album 3" }
+            ]
+          }}
+        />
+      </div>
+    </Container>
   )
 }
