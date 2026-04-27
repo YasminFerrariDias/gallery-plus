bf42c67e68de6d9851da91ac68eb5d9e17c3c083
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-27 14:24:31 -0300

    Estruturando roteamento

---
+++ src/pages/layout-main.tsx
@@ -0,0 +1,9 @@
+import { Outlet } from "react-router";
+import Text from "../components/text";
+
+export default function LayoutMain() {
+  return <>
+    <Text variant="heading-large">Layout Main</Text>
+    <Outlet />
+  </>
+}
---
+++ src/pages/page-photo-details.tsx
@@ -0,0 +1,14 @@
+import { useParams } from "react-router";
+import Text from "../components/text";
+
+export default function PagePhotoDetails() {
+  const {id} = useParams();
+
+  return (
+    <>
+      <Text variant="heading-medium">Detalhe da foto</Text>
+      <hr />
+      <Text variant="heading-medium">ID da foto: {id}</Text>
+    </>
+  )
+}
