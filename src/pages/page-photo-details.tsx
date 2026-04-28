658d35155e27ce65177c130d0b075fa082ccbb51
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-28 11:38:55 -0300

    Mock da página de detalhes da foto - Lista de álbuns selecionáveis

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -6,6 +6,7 @@ import Skeleton from "../components/skeleton";
 import PhotosNavigator from "../contexts/photos/components/photos-navigator";
 import ImagePreview from "../components/image-preview";
 import Button from "../components/button";
+import AlbumsListSelectable from "../contexts/albums/components/albums-list-selectable";
 
 export default function PagePhotoDetails() {
   const { id } = useParams();
@@ -23,12 +24,11 @@ export default function PagePhotoDetails() {
         { id: " 3341", title: "Album 3" }
       ]
     } as Photo
-
   return (
     <Container>
       <header className="flex items-center justify-between gap-8 mb-8">
         {!isLoadingPhoto ? (
-          <Text variant="heading-large">{photo?.title}</Text>
+          <Text as="h2" variant="heading-large">{photo?.title}</Text>
         ) : (
           <Skeleton className="w-48 h-8" />
         )}
@@ -36,7 +36,7 @@ export default function PagePhotoDetails() {
         <PhotosNavigator loading={isLoadingPhoto} />
       </header>
 
-      <div className="grid grid-cols-[21rem] gap-24">
+      <div className="grid grid-cols-[21rem_1fr] gap-24">
         <div className="space-y-3">
 
           {!isLoadingPhoto ? (
@@ -58,7 +58,20 @@ export default function PagePhotoDetails() {
               className="w-20 h-10"
             />
           )}
+        </div>
 
+        <div className="py-3">
+          <Text as="h3" variant="heading-medium" className="mb-6">
+            Álbuns
+          </Text>
+
+          <AlbumsListSelectable photo={photo} albums={[
+            { id: " 3421", title: "Album 1" },
+            { id: " 3426", title: "Album 2" },
+            { id: " 3341", title: "Album 3" }
+          ]}
+          loading={isLoadingPhoto}
+          />
         </div>
       </div>
     </Container>
