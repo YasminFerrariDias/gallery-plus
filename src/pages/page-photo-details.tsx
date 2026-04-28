c9c6cb4df60846b900f9025669a6d070e7874a3c
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-28 10:49:27 -0300

    Mock da página de detalhes da foto - Header da página

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,14 +1,38 @@
 import { useParams } from "react-router";
+import Container from "../components/container";
 import Text from "../components/text";
+import type { Photo } from "../contexts/photos/models/foto";
+import Skeleton from "../components/skeleton";
+import PhotosNavigator from "../contexts/photos/components/photos-navigator";
 
 export default function PagePhotoDetails() {
-  const {id} = useParams();
+  const { id } = useParams();
+
+  // Apenas para fazer o teste do mock
+  const isLoadingPhoto = false;
+  const photo =
+    {
+      id: '123',
+      title: "Olá mundo!",
+      imageId: "portrait-tower.png",
+      albums: [
+        { id: " 3421", title: "Album 1" },
+        { id: " 3426", title: "Album 2" },
+        { id: " 3341", title: "Album 3" }
+      ]
+    } as Photo
 
   return (
-    <>
-      <Text variant="heading-medium">Detalhe da foto</Text>
-      <hr />
-      <Text variant="heading-medium">ID da foto: {id}</Text>
-    </>
+    <Container>
+      <header className="flex items-center justify-between gap-8 mb-8">
+        {!isLoadingPhoto ? (
+          <Text variant="heading-large">{photo?.title}</Text>
+        ) : (
+          <Skeleton className="w-48 h-8" />
+        )}
+
+        <PhotosNavigator loading={isLoadingPhoto} />
+      </header>
+    </Container>
   )
 }
